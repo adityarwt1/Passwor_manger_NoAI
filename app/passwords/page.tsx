7185ba41818087763be3d727a25a39d822f4e141
adityarwt1 <adityarawatnew2487@gmail.com>
2025-04-09 21:17:58 +0530

    password vault completed

--- app/passwords/page.tsx
+++ app/passwords/page.tsx
@@ -1,9 +1,54 @@
+import PasswordInVault from '@/components/PasswordInVault'
 import React from 'react'
 
 const page = () => {
   return (
-    <div className='w-1/2 bg-red-50 border  rounded-md m-5'>
-      sdfsadfasdgg
+    <div className='flex justify-center items-center'>
+    <div className='w-1/2  border  rounded-md m-5 shadow-md'>
+      <div className='flex justify-between border border-gray-200 m-4 p-2 shadow rounded-sm '>
+        <div className='mx-2   w-full flex justify-start '>Platform</div>
+        <div className='mx-2   w-full flex justify-end'>Password</div>
+        <div className='mx-2   w-full flex justify-end'>Option</div>
+      </div>
+      <PasswordInVault/>
+      <PasswordInVault/>
+      <PasswordInVault/>
+      <PasswordInVault/>
+      <PasswordInVault/>
+      <PasswordInVault/>
+      <PasswordInVault/>
+      <PasswordInVault/>
+      <PasswordInVault/>
+      <PasswordInVault/>
+      <PasswordInVault/>
+      <PasswordInVault/>
+      <PasswordInVault/>
+      <PasswordInVault/>
+      <PasswordInVault/>
+      <PasswordInVault/>
+      <PasswordInVault/>
+      <PasswordInVault/>
+      <PasswordInVault/>
+      <PasswordInVault/>
+      <PasswordInVault/>
+      <PasswordInVault/>
+      <PasswordInVault/>
+      <PasswordInVault/>
+      <PasswordInVault/>
+      <PasswordInVault/>
+      <PasswordInVault/>
+      <PasswordInVault/>
+      <PasswordInVault/>
+      <PasswordInVault/>
+      <PasswordInVault/>
+      <PasswordInVault/>
+      <PasswordInVault/>
+      <PasswordInVault/>
+      <PasswordInVault/>
+      <PasswordInVault/>
+      <PasswordInVault/>
+      <PasswordInVault/>
+    </div>
     </div>
   )
 }
---
+++ components/PasswordInVault.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PasswordInVault = () => {
+  return (
+    <div className='flex justify-between border-t border-b border-zinc-950 m-4 p-2 hover:bg-zinc-950 hover:text-white hover:scale-105 duration-300  '>
+        <div className='mx-2   w-full flex justify-start '>valorant</div>
+        <div className='mx-2   w-full flex justify-end'>............</div>
+        <div className='mx-2   w-full flex justify-end'>:</div>
+      </div>
+  )
+}
+
+export default PasswordInVault
