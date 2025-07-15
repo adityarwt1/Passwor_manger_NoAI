553d81ec0f8d48b14fc99231eb754a1b56f0c4b1
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-07-15 21:35:24 +0530

    fetch password api writeen

--- app/api/fetchPassword/route.ts
+++ app/api/fetchPassword/route.ts
@@ -1,51 +1,26 @@
-import mongoose from "mongoose";
-import connectDB from '@/lib/mongodb.js'
+import connectDB from "@/lib/mongodb";
+import Password from "@/models/Password";
 import { NextRequest, NextResponse } from "next/server";
-import User from '@/models/User.js'
 
-export async function GET(req: NextRequest) {
-    try {
-        await connectDB();
-        const searchParams = req.nextUrl.searchParams;
-        const username = searchParams.get("username")
-        const query = searchParams.get("query")
-
-
-        const filter: any = {}
-        if (username) {
-            filter.username = username
-        }
-
-        if (query) {
-            filter.$or = [
-                {
-                    plateform: {
-                        $in: [new RegExp(query, 'i')]
-                    }
-                },
-                {
-                    'plateFormPassword.plateform': {
-                        $regex: query,
-                        $options: 'i'
-                    }
-                }
-            ];
-        }
-
-
-        const passwords = await User.find(filter)
-        if (!passwords) {
-            return NextResponse.json({ success: false, message: "no one password found" }, { status: 404 })
-        }
-
-
-
-        return NextResponse.json({ success: true, data: passwords }, { status: 200 });
-    } catch (error) {
-        console.error("Token verification error:", error);
-        return NextResponse.json(
-            { success: false, message: 'Invalid or expired token' },
-            { status: 401 }
-        );
+export async function POST(req: NextRequest) {
+  try {
+    await connectDB();
+    const username = req.nextUrl.searchParams.get("username");
+    if (!username) {
+      return NextResponse.json({ message: "Bad request" }, { status: 400 });
     }
+
+    const password = await Password.find({ username });
+    if (!password) {
+      return NextResponse.json({ password: [] }, { status: 200 });
+    } else {
+      return NextResponse.json({ password }, { status: 200 });
+    }
+  } catch (error) {
+    console.log((error as Error).message);
+    return NextResponse.json(
+      { mesage: "Internal server issue" },
+      { status: 500 }
+    );
+  }
 }
