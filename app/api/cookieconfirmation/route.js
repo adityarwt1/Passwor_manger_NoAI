bf48b852056369378dea969a20be123fc853992d
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-08-09 12:04:28 +0530

    cookie confirmaiton

--- app/api/cookieconfirmation/route.js
+++ app/api/cookieconfirmation/route.js
@@ -2,33 +2,50 @@ import jwt from "jsonwebtoken";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
-const crosHeader  =  {
-"Access-Control-Allow-Origin": "*",
-"Access-Control-Allow-Methods": "POST",
-"Access-Control-Allow-Headers": "Content-Type, Authorization",
-}
 export async function GET() {
   try {
     const cookiesStore = await cookies();
     const token = cookiesStore.get("token")?.value;
     console.log(token);
     if (!token) {
-      const respone =  NextResponse.json(
-        { success: false, message: "Unotherized route" },
-        { status: 401 }
-      );
-      respone.headers(crosHeader)
-      return respone
+return NextResponse.json(
+  { success: false, message: "Unotherized route" },
+  {
+    status: 401,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "POST",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  }
+);
     }
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     if (decoded) {
-      const response =  NextResponse.json({ success: true, data: true }, { status: 200 });
-      response.headers(crosHeader)
-      return response
+return NextResponse.json(
+  { success: true, data: true },
+  {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "POST",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  }
+);
     }
   } catch (error) {
-    const response = NextResponse.json({ success: false, message: error });
-    response.headers(crosHeader)
+    const response = NextResponse.json(
+      { success: false, message: error },
+      {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "POST",
+          "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        },
+        status  : 500
+      }
+    );
     return response
   }
 }
