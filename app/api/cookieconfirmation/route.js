686b8d640d427815ca21425afd3dd541d6cfdb4f
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-08-09 12:01:28 +0530

    coookie confirmantion page

--- app/api/cookieconfirmation/route.js
+++ app/api/cookieconfirmation/route.js
@@ -2,22 +2,33 @@ import jwt from "jsonwebtoken";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
+const crosHeader  =  {
+"Access-Control-Allow-Origin": "*",
+"Access-Control-Allow-Methods": "POST",
+"Access-Control-Allow-Headers": "Content-Type, Authorization",
+}
 export async function GET() {
   try {
     const cookiesStore = await cookies();
     const token = cookiesStore.get("token")?.value;
     console.log(token);
     if (!token) {
-      return NextResponse.json(
+      const respone =  NextResponse.json(
         { success: false, message: "Unotherized route" },
         { status: 401 }
       );
+      respone.headers(crosHeader)
+      return respone
     }
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     if (decoded) {
-      return NextResponse.json({ success: true, data: true }, { status: 200 });
+      const response =  NextResponse.json({ success: true, data: true }, { status: 200 });
+      response.headers(crosHeader)
+      return response
     }
   } catch (error) {
-    return NextResponse.json({ success: false, message: error });
+    const response = NextResponse.json({ success: false, message: error });
+    response.headers(crosHeader)
+    return response
   }
 }
