d3d0ba1a93926a8716a37c04a52cc20cbf94c956
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-08-09 12:05:36 +0530

    delete api now cros

--- app/api/delete/route.ts
+++ app/api/delete/route.ts
@@ -7,25 +7,56 @@ export async function DELETE(req: NextRequest) {
     await connectDB();
     const { _id } = await req.json();
     if (!_id) {
-      return NextResponse.json({ message: "bad request" }, { status: 400 });
+      return NextResponse.json(
+        { message: "bad request" },
+        {
+          status: 400,
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "POST",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+          },
+        }
+      );
     }
     const password = await Password.findOneAndDelete({ _id });
     if (!password) {
       return NextResponse.json(
         { message: "passowrd not found" },
-        { status: 404 }
+        {
+          status: 404,
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "POST",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+          },
+        }
       );
     } else {
       return NextResponse.json(
         { message: "Password delete successfully" },
-        { status: 200 }
+        {
+          status: 200,
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "POST",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+          },
+        }
       );
     }
   } catch (error) {
     console.log((error as Error).message);
     return NextResponse.json(
       { message: "Internal server issue" },
-      { status: 500 }
+      {
+        status: 500,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "POST",
+          "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        },
+      }
     );
   }
 }
